ff5cefb72c459057b7a6069c5eda09dad5dc0ac2
maydaychen <cy245832451@outlook.com>
2017-08-03 18:09:34 +0800

    项目上传

--- app/src/main/java/com/example/user/guokun/MainActivity.java
+++ app/src/main/java/com/example/user/guokun/MainActivity.java
@@ -6,8 +6,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.example.user.guokun.ui.activity.InitActivity;
 import com.example.user.guokun.ui.fragment.MainFragment;
 import com.example.user.guokun.ui.widget.CustomViewPager;
 
@@ -17,7 +18,7 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends InitActivity {
 
     @BindView(R.id.vp_content)
     CustomViewPager mContentVp;
@@ -26,25 +27,32 @@ public class MainActivity extends AppCompatActivity {
 
     private List<Fragment> tabFragments;
     private List<String> tabIndicators;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void initView(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initContent();
+        initTab();
     }
+
+    @Override
+    public void initData() {
+
+    }
+
+
     private void initContent() {
         tabIndicators = new ArrayList<>();
-        tabIndicators.add("首页");
-        tabIndicators.add("健康金");
-        tabIndicators.add("购药");
-        tabIndicators.add("寻医问药");
-        tabIndicators.add("我");
+        tabIndicators.add("按摩");
+        tabIndicators.add("发现");
+        tabIndicators.add("个人中心");
+
         tabFragments = new ArrayList<>();
         tabFragments.add(MainFragment.newInstance(tabIndicators.get(0)));
         tabFragments.add(MainFragment.newInstance(tabIndicators.get(1)));
         tabFragments.add(MainFragment.newInstance(tabIndicators.get(2)));
-        tabFragments.add(MainFragment.newInstance(tabIndicators.get(3)));
-        tabFragments.add(MainFragment.newInstance(tabIndicators.get(4)));
+
         ContentPagerAdapter contentAdapter = new ContentPagerAdapter(getSupportFragmentManager());
         mContentVp.setAdapter(contentAdapter);
     }
@@ -57,32 +65,32 @@ public class MainActivity extends AppCompatActivity {
         mTabTl.setupWithViewPager(mContentVp);
         for (int i = 0; i < tabIndicators.size(); i++) {
             TabLayout.Tab itemTab = mTabTl.getTabAt(i);
-//            if (itemTab != null) {
-//                switch (i) {
-//                    case 0:
-//                        itemTab.setCustomView(R.layout.item_tab_layout_custom);
-//                        TextView itemTv = (TextView) itemTab.getCustomView().findViewById(R.id
-//                                .tv_menu_item);
-//                        itemTv.setText(tabIndicators.get(i));
-//                        break;
-//                    case 1:
-//                        itemTab.setCustomView(R.layout.item_tab_layout_jkj);
-//                        TextView itemTv1 = (TextView) itemTab.getCustomView().findViewById(R.id
-//                                .tv_menu_item1);
-//                        itemTv1.setText(tabIndicators.get(i));
-//                        break;
-//                    case 2:
-//                        itemTab.setCustomView(R.layout.item_tab_layout_jksc);
-//                        TextView itemTv2 = (TextView) itemTab.getCustomView().findViewById(R.id
-//                                .tv_menu_item2);
-//                        itemTv2.setText(tabIndicators.get(i));
-//                        break;
-//                }
-////                itemTab.setCustomView(R.layout.item_tab_layout_custom);
-////                TextView itemTv = (TextView) itemTab.getCustomView().findViewById(R.id
-//// .tv_menu_item);
-////                itemTv.setText(tabIndicators.get(i));
-//            }
+            if (itemTab != null) {
+                switch (i) {
+                    case 0:
+                        itemTab.setCustomView(R.layout.item_tab_layout_custom);
+                        TextView itemTv = (TextView) itemTab.getCustomView().findViewById(R.id
+                                .tv_menu_item);
+                        itemTv.setText(tabIndicators.get(i));
+                        break;
+                    case 1:
+                        itemTab.setCustomView(R.layout.item_tab_layout_jkj);
+                        TextView itemTv1 = (TextView) itemTab.getCustomView().findViewById(R.id
+                                .tv_menu_item1);
+                        itemTv1.setText(tabIndicators.get(i));
+                        break;
+                    case 2:
+                        itemTab.setCustomView(R.layout.item_tab_layout_jksc);
+                        TextView itemTv2 = (TextView) itemTab.getCustomView().findViewById(R.id
+                                .tv_menu_item2);
+                        itemTv2.setText(tabIndicators.get(i));
+                        break;
+                }
+//                itemTab.setCustomView(R.layout.item_tab_layout_custom);
+//                TextView itemTv = (TextView) itemTab.getCustomView().findViewById(R.id
+// .tv_menu_item);
+//                itemTv.setText(tabIndicators.get(i));
+            }
         }
     }
 
